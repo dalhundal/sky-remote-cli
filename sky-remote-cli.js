2c1a01f4bb515279221631a597fa9ff71b75001f
Dal Hundal <dal@dalhundal.com>
2016-01-29 12:24:21 +0000

    BUGFIX: wasn't using ip from arguments

--- sky-remote-cli.js
+++ sky-remote-cli.js
@@ -3,8 +3,8 @@
 var SkyRemote = require('sky-remote');
 
 var args = process.argv.splice(2);
-var ip = args.splice(0,1);
+var ip = args.splice(0,1)[0];
 var commands = args;
 
-var remoteControl = new SkyRemote('192.168.0.40');
+var remoteControl = new SkyRemote(ip);
 remoteControl.press(commands);
